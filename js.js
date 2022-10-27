c1fb854695489754dc2033ff00aacf3981797eb4
drewherald <drewherald9@gmail.com>
2022-10-27 11:14:21 -0400

    added rollover functionality with altText for all IMGs
    need to clean code w/loops

--- js.js
+++ js.js
@@ -1,17 +1,132 @@
 const img1 = document.querySelector("#milan");
-img1.addEventListener("mouseover", (event) => {
-    let paragraph = document.createElement("p");
-    let altText = document.createTextNode("milan, 2022");
-    paragraph.setAttribute("class","altText");
-    paragraph.setAttribute("style","visibility: visible;")
-    paragraph.appendChild(altText);
-    document.querySelector(".photo1").appendChild(paragraph);
-})
+const par = document.querySelector("#altText1");
 
-img1.addEventListener("mouseout", (event) => {
-    let paragraph = document.createElement("p");
-    let altText = document.createTextNode("milan, 2022");
-    paragraph.setAttribute("class","altText");
-    paragraph.appendChild(altText);
-    document.querySelector(".photo1").appendChild(paragraph);
-})
+img1.addEventListener("mouseover", function (e) {
+    par.style.visibility = 'visible';
+    });
+
+img1.addEventListener("mouseout", function (e) {
+    par.style.visibility = 'hidden';
+    });
+
+const img2 = document.querySelector("#florence");
+const par2 = document.querySelector("#altText2");
+
+img2.addEventListener("mouseover", function (e) {
+    par2.style.visibility = 'visible';
+    });
+
+img2.addEventListener("mouseout", function (e) {
+    par2.style.visibility = 'hidden';
+    });
+
+const img3 = document.querySelector("#vienna");
+const par3 = document.querySelector("#altText3");
+  
+img3.addEventListener("mouseover", function (e) {
+    par3.style.visibility = 'visible';
+    });
+  
+img3.addEventListener("mouseout", function (e) {
+    par3.style.visibility = 'hidden';
+    });
+
+const img4 = document.querySelector("#venice");
+const par4 = document.querySelector("#altText4");
+    
+img4.addEventListener("mouseover", function (e) {
+    par4.style.visibility = 'visible';
+    });
+    
+img4.addEventListener("mouseout", function (e) {
+    par4.style.visibility = 'hidden';
+    });
+
+const img5 = document.querySelector("#malta");
+const par5 = document.querySelector("#altText5");
+      
+img5.addEventListener("mouseover", function (e) {
+    par5.style.visibility = 'visible';
+    });
+      
+img5.addEventListener("mouseout", function (e) {
+    par5.style.visibility = 'hidden';
+    });
+
+const img6 = document.querySelector("#malta2");
+const par6 = document.querySelector("#altText6");
+        
+img6.addEventListener("mouseover", function (e) {
+    par6.style.visibility = 'visible';
+    });
+        
+img6.addEventListener("mouseout", function (e) {
+    par6.style.visibility = 'hidden';
+    });
+        
+const img7 = document.querySelector("#malta3");
+const par7 = document.querySelector("#altText7");
+    
+img7.addEventListener("mouseover", function (e) {
+    par7.style.visibility = 'visible';
+    });
+    
+img7.addEventListener("mouseout", function (e) {
+    par7.style.visibility = 'hidden';
+    });
+    
+const img8 = document.querySelector("#marrakech");
+const par8 = document.querySelector("#altText8");
+    
+img8.addEventListener("mouseover", function (e) {
+    par8.style.visibility = 'visible';
+    });
+    
+img8.addEventListener("mouseout", function (e) {
+    par8.style.visibility = 'hidden';
+    });
+    
+const img9 = document.querySelector("#interlaken");
+const par9 = document.querySelector("#altText9");
+        
+img9.addEventListener("mouseover", function (e) {
+    par9.style.visibility = 'visible';
+    });
+        
+img9.addEventListener("mouseout", function (e) {
+    par9.style.visibility = 'hidden';
+    });
+    
+const img10 = document.querySelector("#milan2");
+const par10 = document.querySelector("#altText10");
+        
+img10.addEventListener("mouseover", function (e) {
+    par10.style.visibility = 'visible';
+    });
+        
+img10.addEventListener("mouseout", function (e) {
+    par10.style.visibility = 'hidden';
+    });
+    
+const img11 = document.querySelector("#berlin");
+const par11 = document.querySelector("#altText11");
+          
+img11.addEventListener("mouseover", function (e) {
+    par11.style.visibility = 'visible';
+    });
+          
+img11.addEventListener("mouseout", function (e) {
+    par11.style.visibility = 'hidden';
+    });
+    
+const img12 = document.querySelector("#milan3");
+const par12 = document.querySelector("#altText12");
+            
+img12.addEventListener("mouseover", function (e) {
+    par12.style.visibility = 'visible';
+    });
+            
+img12.addEventListener("mouseout", function (e) {
+    par12.style.visibility = 'hidden';
+    });
+            
